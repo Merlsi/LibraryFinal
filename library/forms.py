8a308accff2b6bc174dfef6b28407745911301b2
Merlsi <102343305+Merlsi@users.noreply.github.com>
2023-05-10 13:55:56 +0900

    Update forms.py

--- library/forms.py
+++ library/forms.py
@@ -18,3 +18,14 @@ class PublisherForm(ModelForm):
     class Meta:
         model = Publisher
         fields = '__all__'
+        
+class MyUserCreationForm(ModelForm):
+    username = forms.CharField(max_length=30, required=True, help_text='Required.')
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    # passwords = forms.CharField('')
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
